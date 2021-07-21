e9e666df6e974aee3166eb665ad36080d6dbded1
Monika Geiger <geigermonika@hotmail.hu>
2021-07-21 11:36:56 +0200

    rules added

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
-     render() {
+  render() {
+    const { score, name, doScore, description } = this.props;
+    const disabled = score !== undefined;
     return (
-      <tr className= {this.props.disabled ? "RuleRow RuleRow-disabled" : "RuleRow RuleRow-active"} onClick={this.props.doScore,this.props.func1}>
-        <td className="RuleRow-name" >{this.props.name} </td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr
+        className={`RuleRow RuleRow-${disabled ? "disabled" : "active"}`}
+        onClick={disabled ? null : doScore}
+      >
+        <td className='RuleRow-name'>{name}</td>
+        <td className='RuleRow-score'>{disabled ? score : description}</td>
       </tr>
-    )
+    );
   }
-    }
+}
 
 export default RuleRow;
